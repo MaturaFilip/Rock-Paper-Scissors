3e37bed67b4576981c9308b4e31424de8fa4a9c2
Filip Mat <matura.filip96@gmail.com>
2024-01-06 18:24:54 +0100

    game() function finished. The final game has 5 round and at the end of the final round prints results.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // Rock-Paper-Scissors game
 
+// score board
+    // won counter
+let win_counter = 0
+
+    // lose counter
+let lose_counter = 0
+
+    // draw counter
+let draw_counter = 0
+
+
+
 // getComputerChoice function
-    
 function getComputerChoice() {
     // return randomly (not cheating) Rock, Paper or Scissors
     // get numbers 0, 1 or 2 randomly
@@ -22,18 +33,31 @@ function getComputerChoice() {
 function playRound(computer, player) {
     // Return string that declares the winner of the round
     if (computer === player) {
+        draw_counter += 1
         return "Draw"
+        
     } else if (computer === "rock" && player === "paper") {
+        win_counter += 1
         return "You won! Paper beats rock"
+
     } else if (computer === "paper" && player === "scissors") {
+        win_counter += 1
         return "You won! Scissors beats paper"
+
     } else if (computer === "scissors" && player === "rock") {
+        win_counter += 1
         return "Your won! Rock beats scissors"
+
     } else if (computer === "rock" && player === "scissors") {
+        lose_counter +=1
         return "You lose! Rock beats scissors"
+
     } else if (computer === "paper" && player === "rock") {
+        lose_counter +=1
         return "You lose! Paper beats rock!"
+
     } else if (computer === "scissors" && player === "paper") {
+        lose_counter +=1
         return "You lose! Scissors beats paper!"
     }
 }
@@ -55,14 +79,36 @@ function game() {
 
         // check proper input
         if (player == "paper"|| player == "rock"|| player == "scissors") {
+
             // execute if the input is in correct 
             alert(playRound(getComputerChoice(), player))
+
+        // Tell the user that input was wrong and lower the i to add more rounds
         } else {
             alert("Wrong input")
             i -= 1
         }
 
     }
+
+    // print the final score after 5 rounds
+    let finalResults;
+    if (win_counter > lose_counter) {
+        finalResults = "You are the total winner!"
+        } else if (lose_counter > win_counter) {
+        finalResults = "You are such a loser!"
+        } else {
+        finalResults = "No one wins!"
+        }
+    
+    alert(`Tha final score:
+
+                wins: ${win_counter}
+                loses: ${lose_counter}
+                draws: ${draw_counter}
+                
+                -----------------------
+                ${finalResults}`)
 }
 
 game()
